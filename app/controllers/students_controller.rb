a53eea404a6eb4ff950d8c7ba7c30ff65df31890
Raghu Betina <rbetina@gmail.com>
2013-01-21 11:27:04 -0600

    Added support for /students URL and made it smart enough to handle the query string parameter ?student=.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,22 @@
 class StudentsController < ApplicationController
 
+  # Support a url like this:
+  # http://localhost:3000/students?section=pm
+
+  def index
+    if params["section"] != nil
+      user_section = params["section"]
+      @students = Student.fetch(user_section)
+    else
+      am_array = Student.fetch("am")
+      pm_array = Student.fetch("pm")
+      @students = am_array + pm_array
+    end
+  end
+
   def morning
     @students = Student.fetch("am")
+    @color = params["color"]
   end
 
   def afternoon
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ YearbookApp::Application.routes.draw do
 
   get("/am", :controller => "Students", :action => "morning")
   get("/pm", :controller => "Students", :action => "afternoon")
-
+  get("/students", :controller => "Students", :action => "index")
 end
