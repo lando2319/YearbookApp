f9176805c306434c4b00571d48e22a8d219e3d82
Raghu Betina <rbetina@gmail.com>
2013-01-21 09:50:32 -0600

    Added Student.fetch class method to retrieve API data.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,25 +1,11 @@
-require 'open-uri'
-
 class StudentsController < ApplicationController
 
   def morning
-    url = "http://tslwebdev.herokuapp.com/am.json"
-    json_string = open(url).read
-    student_hashes = JSON.parse(json_string)
-
-    @students = Array.new
-    student_hashes.each do |student_hash|
-      s = Student.new(student_hash["name"],
-                      student_hash["twitter"],
-                      student_hash["section"],
-                      student_hash["photo_url"])
-
-      @students << s
-    end
+    @students = Student.fetch("am")
   end
 
   def afternoon
-
+    @students = Student.fetch("pm")
   end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,28 @@
+# require 'open-uri'
+# Does Rails 3.2 automatically do this too?
+
 class Student
   attr_accessor "name"
   attr_accessor "photo_url"
   attr_accessor "section"
   attr_accessor "twitter"
+
+  def Student.fetch(section)
+    url = "http://tslwebdev.herokuapp.com/#{section}.json"
+    json_string = open(url).read
+    student_hashes = JSON.parse(json_string)
+
+    students = Array.new
+    student_hashes.each do |student_hash|
+      s = Student.new(student_hash["name"],
+                      student_hash["section"],
+                      student_hash["photo_url"],
+                      student_hash["twitter"])
+      students << s
+    end
+    return students
+  end
+
 
   def initialize(the_name, the_section, pic_url, handle)
     @name = the_name
