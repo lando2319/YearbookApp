c1fa3964eeddf62cfb140f4368febb2a39cc1f9a
Raghu Betina <rbetina@gmail.com>
2013-01-21 08:58:32 -0600

    Added Student class to models folder.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,8 +5,18 @@ class StudentsController < ApplicationController
   def morning
     url = "http://tslwebdev.herokuapp.com/am.json"
     json_string = open(url).read
+    student_hashes = JSON.parse(json_string)
 
-    @students = JSON.parse(json_string)
+    @students = Array.new
+    student_hashes.each do |student_hash|
+      s = Student.new
+      s.name = student_hash["name"]
+      s.twitter = student_hash["twitter"]
+      s.section = student_hash["section"]
+      s.photo_url = student_hash["photo_url"]
+
+      @students << s
+    end
   end
 
   def afternoon
